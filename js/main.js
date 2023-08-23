ff7b70a93477bd6d440284a6fca091d156859e63
jay <125956228+j-rhee@users.noreply.github.com>
2023-08-23 21:52:31 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
+// swiper
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
+  slidesPerView: 5,
   spaceBetween: 36,
   autoplay: true,
   loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-  },
 });
 
-const btn = document.querySelector('.btn-menu');
-const nav = document.querySelector('#nav');
-
-btn.addEventListener('click', () => {
-  nav.classList.toggle('open-menu');
+// fade-in
+$(document).ready(function () {
+  $(window).scroll(function () {
+    $('.fade').each(function (i) {
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ 'opacity': '1' }, 900);
+      }
+    });
+  });
 });
